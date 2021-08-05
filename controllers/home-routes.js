278051b7ae26057d969e2727ff582663e60b17a4
panicor <rachelpanico@hotmail.com>
2021-08-05 12:53:39 -0400

    new data

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -11,7 +11,7 @@ router.get('/', async (req, res) => {
   
       const users = userData.map((el) => el.get({ plain: true }));
   
-      res.render('main', {
+      res.render('layouts/main', {
         users,
         logged_in: req.session.logged_in,
       });
