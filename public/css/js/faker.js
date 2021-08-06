019c561f2aafd524b0531a90850c165cc7b3becb
panicor <rachelpanico@hotmail.com>
2021-08-06 10:02:22 -0400

    added faker.js

--- public/css/js/faker.js
+++
@@ -1,9 +0,0 @@
-_.times(5, function(n) {
-    return {
-      id: n,
-      name: faker.name.findName(),
-      salary: faker.random.number(),
-      country: faker.address.country(),
-      city: faker.address.city()
-    }
-  })
